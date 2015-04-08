d70f3930c75069b6a15825eee0b0a115cceafec2
Yohanan <yohananjr13@gmail.com>
2015-04-08 23:39:17 +0700

    hello world - lesson 2

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,10 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageView:UIImageView = UIImageView()
+    var backgroundDict: Dictionary<String, String> = ["Background1": "background1", "Background2": "background2"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let width:CGFloat = 320
+        let height: CGFloat = 568
+        
+        backgroundDict = ["Background1": "background1", "Background2": "background2"]
+        
+        var buttonTitles = ["Backround 1", "Background 2"]
+        
+        var view:UIView = UIView(frame: CGRectMake(0, 0, width, height))
+        
+        self.view.addSubview(view)
+        
+        var backgroundImage:UIImage = UIImage(named:backgroundDict["background1"]!)!
+        
+        imageView = UIImageView(frame: view.frame)
+        
+        imageView.image = backgroundImage
+        
+        view.addSubview(imageView)
+        
+        for i in 0 ... buttonTitles.count - 1{
+            var button:UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+            
+            button.frame = CGRectMake(0, 0, 200, 20)
+            
+            button.center = CGPointMake(view.center.x, CGFloat(100+i*50))
+            
+            button.setTitle(buttonTitles[i], forState: UIControlState.Normal)
+            
+            button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            
+            button.addTarget(self, action: "buttonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
+            
+            button.tag = i + 1
+            view.addSubview(button)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
